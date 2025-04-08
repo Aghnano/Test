ad5b24960137f114b5595fd2de81f57ff3d8d33c
Aghnano <agnese.arenaccio@me.com>
2025-04-08 12:10:22 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import EditableMondayTable from './EditableMondayTable';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EditableMondayTable />
+  </React.StrictMode>
+);
